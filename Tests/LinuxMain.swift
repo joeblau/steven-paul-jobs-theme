524feb690c6099d99b60715f7a46a1af80935bab
Joe Blau <im@joeblau.com>
2020-01-11 14:36:46 -0800

    Remove tests

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import StevenPaulJobsThemeTests
-
-var tests = [XCTestCaseEntry]()
-tests += StevenPaulJobsThemeTests.allTests()
-XCTMain(tests)
--- Tests/StevenPaulJobsThemeTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(StevenPaulJobsThemeTests.allTests),
-    ]
-}
-#endif
